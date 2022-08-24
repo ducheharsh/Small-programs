3ad523b26c1842624da5c7c15be2ca4bfbdb8969
Harsh Duche <ducheharsh@gmail.com>
2022-08-24 15:59:32 +0530

    Feature added in Guess method

--- guess_method.py
+++ guess_method.py
@@ -2,12 +2,14 @@
  
 # This is small python program to find cube root using Guess method
   
-cube = int(input("Enter a number"))
+cube = int(input("Enter a number: "))
 guess = 0
 ep = 0.001
 incr = 1
-while  abs(guess**3 - cube) >= ep:
+while abs(guess**3 - cube) >= ep and guess <= cube: 
     guess += incr
-print(guess , "is the closest one")
-        
+if abs(guess**3 - cube) >= ep:
+    print("Not a perfect cube")
+else:
+    print (guess , "is the cube root of" , cube)
     
